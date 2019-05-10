a4a05633548a7902ab50c47f2a2dd7a3caaae309
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-05-10 08:33:51 -0700

    initial setup

---
+++ HairSalon/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View(Item.GetAll());
+      }
+
+    }
+}
