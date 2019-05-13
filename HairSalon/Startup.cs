0d931d02fd4b493a2e884596c8481fc060d5477d
Dustin Herboldshimer and zsuzsanna mangu <zsuzsanna@>
2019-05-13 12:02:28 -0700

    update csproj packages, add static file root and css/styles.css

--- HairSalon/Startup.cs
+++ HairSalon/Startup.cs
@@ -25,6 +25,8 @@ namespace HairSalon
 
     public void Configure(IApplicationBuilder app)
     {
+      app.UseDeveloperExceptionPage();
+      app.UseStaticFiles();
       app.UseMvc(routes =>
       {
         routes.MapRoute(
